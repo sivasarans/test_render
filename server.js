0e1375f0e06a50b608c2f8b477e77f1c8cebc916
Sivasaran <146517374+sivasarans@users.noreply.github.com>
2025-04-01 11:52:08 +0530

    Update server.js
    apr 1 2025 updated

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
-// Import the HTTP module
 const http = require('http');
 
-// Define the port to listen on
 const PORT = 3000;
 
-// Create the server
 const server = http.createServer((req, res) => {
-    // Log the request method and URL
     console.log(`Received ${req.method} request for: ${req.url}`);
 
-    // Set the response header and content
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World!\n');
+
+    if (req.url === '/') {
+        res.end('Hello, World!\n');
+    } else if (req.url === '/test1') {
+        res.end('You reached /test1\n');
+    } else if (req.url === '/test2') {
+        res.end('You reached /test2\n');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found\n');
+    }
 });
 
-// Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
